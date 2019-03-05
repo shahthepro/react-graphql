1fe5b65662ae7722dfc355cd31065d794585edf9
Shah <shahthepro@outlook.com>
2019-03-05 21:45:19 +0530

    Add test query/mtutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutation = {};
+const Mutation = {
+  createDog(parent, args, context, info) {
+    global.dogs = global.dogs || [];
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
 
 module.exports = Mutation;
