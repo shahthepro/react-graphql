0647d64789de6e32389c3226c32e9152e5fcc3e8
Shah <shahthepro@outlook.com>
2019-03-10 19:13:11 +0530

    Add User component

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,9 +1,15 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
-
+import User from './User';
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({ data: { me } }) => {
+        if (me) return <p>{ me.name }</p>
+        return <p>Not logged in</p>
+      }}
+    </User>
     <Link href="/">
       <a>Home</a>
     </Link>
