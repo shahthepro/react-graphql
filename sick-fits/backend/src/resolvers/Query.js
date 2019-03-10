d446541fa7e8d204792cc1034d072b6cadf51295
Shah <shahthepro@outlook.com>
2019-03-10 18:38:16 +0530

    Add me query and decode JWT

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -4,6 +4,15 @@ const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
+  async me(parent, args, context, info) {
+    if (!context.request.userId) {
+      return null;
+    }
+
+    const user = await context.db.query.user({ where: { id: context.request.userId }}, info);
+
+    return user;
+  },
   // async items(parent, args, context, info) {
   //   const items = await context.db.query.items();
   //   return items;
