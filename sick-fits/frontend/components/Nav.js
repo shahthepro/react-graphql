4c1556e6884cbc71b400b74287937c8376075a8d
Shah <shahthepro@outlook.com>
2019-03-10 19:50:56 +0530

    Update Nav menu based on signin

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,32 +3,36 @@ import NavStyles from './styles/NavStyles';
 import User from './User';
 
 const Nav = () => (
-  <NavStyles>
-    <User>
-      {({ data: { me } }) => {
-        if (me) return <p>{ me.name }</p>
-        return <p>Not logged in</p>
-      }}
+  <User>
+      {({ data: { me } }) => (
+        <NavStyles>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/items">
+            <a>Shop</a>
+          </Link>
+          { me && (
+            <>
+              <Link href="/sell">
+                <a>Sell</a>
+              </Link>
+              <Link href="/orders">
+                <a>Orders</a>
+              </Link>
+              <Link href="/me">
+                <a>Account</a>
+              </Link>
+            </>
+          )}
+          { !me && (
+            <Link href="/signup">
+              <a>Sign in</a>
+            </Link>
+          )}
+        </NavStyles>
+      )}
     </User>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyles>
 );
 
 export default Nav;
